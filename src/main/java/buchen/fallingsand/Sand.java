bf73d9b0f0f634ffa2bb6c2adfd39f800d1e855f
Aliza Buchen <alizabuchen@alizas-air-2.lan>
2024-03-05 12:48:47 -0500

    more updates

--- src/main/java/buchen/fallingsand/Sand.java
+++ src/main/java/buchen/fallingsand/Sand.java
@@ -3,23 +3,23 @@ package buchen.fallingsand;
 public class Sand {
     private final int[][] field = new int[3][3];
 
-    public String toString ()
+    public String toString  ()
     {
-        StringBuilder builder = new StringBuilder ();
+        StringBuilder builder = new StringBuilder  ();
 
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
-                builder.append (field[y][x]);
+                builder.append  (field[y][x]);
             }
-            builder.append ("\n");
+            builder.append  ("\n");
         }
-        return builder.toString ();
+        return builder.toString  ();
     }
 
     /**
      * @return the value in field
      */
-    public int get (int x, int y)
+    public int get  (int x, int y)
     {
         return field[y][x];
     }
@@ -27,12 +27,12 @@ public class Sand {
     /**
      * Sets the value in field to be 1
      */
-    public void put (int x, int y)
+    public void put  (int x, int y)
     {
         field[y][x] = 1;
     }
 
-    public void fall () {
+    public void fall  () {
         //moves all sand down one square
         //if sand at bottom level doesn't go down
         //if sand falls and sand below it cant go down
@@ -40,7 +40,7 @@ public class Sand {
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
                 if (field [y][x] == 1 && y < 2 && field [y + 1][x] == 0) {
-                    put (x, y + 1);
+                    put  (x, y + 1);
                     field [y][x] = 0;
                     fell = true;
                 }
